53cad4554dfde99c8fbdd49112219479738a895e
Cinna-Winter <146698712+Cinna-Winter@users.noreply.github.com>
2024-05-28 14:18:46 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -11,6 +11,33 @@ echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
-
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+#Step 2
+if [[ -f student-submission/ListExamples.java ]]
+then 
+  echo "ListExamples.java file found."
+else 
+  echo "ListExamples.java file not found."
+  echo "Grade: 0"
+  exit 
+fi
+
+#Step 3
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+#Step 4
+cd grading-area
+# echo $(ls)
+javac -cp $CPATH *.java
+echo "The exit code for the compilation is $?"
+if [[ $? -ne 0 ]]
+then
+  echo "Grade: 0"
+  exit
+fi
+
+#Step 5
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
